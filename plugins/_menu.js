5b09febe51daef536bb3046d16c817cd9ca3cf99
Neeraj <krishnaneeraj774@gmail.com>
2022-09-14 21:14:44 +0530

    more updates

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -8,7 +8,7 @@ command(
     desc: "Show All commands",
   },
   async (message, match) => {
-    let menu = "";
+    let menu = `╭╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼\n╽`
     let cmnd = [];
     events.commands.map((command, num) => {
       let cmd;
@@ -18,32 +18,15 @@ command(
           .match(/(\W*)([A-Za-züşiğ öç1234567890]*)/)[2];
       }
 
-      if (!command.dontAddCommandList&&cmd !==undefined) {
+      if (!command.dontAddCommandList&& cmd !==undefined) {
         cmnd.push(cmd);
       }
     });
     cmnd.sort();
     cmnd.forEach((cmd, num) => {
-      menu += `${(num += 1)} ${cmd} \n`;
+      menu += `\n┠${(num += 1)} \`\`\`${cmd}\`\`\` \n╿`;
     });
-    message.sendMessage(menu, {
-      quoted: {
-        key: {
-          participant: "0@s.whatsapp.net",
-          remoteJid: "status@broadcast",
-        },
-        message: {
-          orderMessage: {
-            itemCount: 123,
-            itemCoun: 404,
-            surface: 404,
-            message: `© X-asena`,
-            orderTitle: "B",
-            thumbnail: { url: "https://imgur.com/MCOhVfX" },
-            sellerJid: "0@s.whatsapp.net",
-          },
-        },
-      },
-    });
+    menu += `\n╰╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼╾╼`
+    message.sendMessage(readFileSync("./media/thumb.jpeg"), {caption:menu},'image');
   }
 );
--- plugins/qr.js
+++ plugins/qr.js
@@ -1,25 +1,23 @@
-const { command } = require('../lib/')
-const jimp = require('jimp')
-const QRReader = require('qrcode-reader')
+const { command, qrcode } = require("../lib/");
+const jimp = require("jimp");
+const QRReader = require("qrcode-reader");
 
 command(
-	{ pattern: 'qr ?(.*)', fromMe: true, desc: 'Read/Write Qr.', type: 'misc' },
-	async (message, match) => {
-		if (match)
-			return await message.sendFromUrl(
-				`https://levanter.up.railway.app/gqr?text=${encodeURIComponent(match)}`
-			)
-		if (!message.reply_message || !message.reply_message.image)
-			return await message.send(
-				'*Example : qr test*\n*Reply to a qr image.*'
-			)
+  { pattern: "qr ?(.*)", fromMe: true, desc: "Read/Write Qr.", type: "misc" },
+  async (message, match) => {
+    match = match || message.reply_message.text;
+    if (match) {
+      let buff = await qrcode(match);
+      return await message.sendMessage(buff, {}, "image");
+    } else if (!message.reply_message || !message.reply_message.image)
+      return await message.send("*Example : qr test*\n*Reply to a qr image.*");
 
-		const { bitmap } = await jimp.read(
-			await message.reply_message.downloadMediaMessage()
-		)
-		const qr = new QRReader()
-		qr.callback = (err, value) =>
-			message.send(err ?? value.result, { quoted: message.data })
-		qr.decode(bitmap)
-	}
-)
+    const { bitmap } = await jimp.read(
+      await message.reply_message.downloadMediaMessage()
+    );
+    const qr = new QRReader();
+    qr.callback = (err, value) =>
+      message.send(err ?? value.result, { quoted: message.data });
+    qr.decode(bitmap);
+  }
+);
