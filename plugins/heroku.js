566724060cf49c1d016156e8a24b0263649967dc
Neeraj <krishnaneeraj774@gmail.com>
2022-09-17 21:14:23 +0530

    Updates Added

--- plugins/heroku.js
+++ plugins/heroku.js
@@ -4,6 +4,10 @@ const { secondsToDHMS, command } = require("../lib/");
 const Config = require("../config");
 const heroku = new Heroku({ token: Config.HEROKU_API_KEY });
 const baseURI = "/apps/" + Config.HEROKU_APP_NAME;
+const simpleGit = require("simple-git");
+const git = simpleGit();
+const Config = require("../config");
+const exec = require("child_process").exec;
 
 command(
   {
@@ -184,5 +188,88 @@ command(
       .catch(async (error) => {
         await message.sendMessage(`HEROKU : ${error.body.message}`);
       });
+  }
+);
+
+command(
+  {
+    pattern: "update$",
+    fromMe: true,
+    desc: "Checks for update.",
+  },
+  async (message) => {
+    await git.fetch();
+    var commits = await git.log([Config.BRANCH + "..origin/" + Config.BRANCH]);
+    if (commits.total === 0) {
+      await message.sendMessage("_Already on latest version_");
+    } else {
+      var updates = "Update Available*\n\n\n Changes:\n```";
+      commits["all"].map((commit) => {
+        updates +=
+          "🔹 [" +
+          commit.date.substring(0, 10) +
+          "]: " +
+          commit.message +
+          " <" +
+          commit.author_name +
+          ">\n";
+      });
+
+      await message.sendMessage(updates + "```");
+    }
+  }
+);
+
+command(
+  {
+    pattern: "update now$",
+    fromMe: true,
+    dontAddCommandList: true,
+    desc: "Updates the Bot",
+  },
+  async (message) => {
+    await git.fetch();
+    var commits = await git.log([Config.BRANCH + "..origin/" + Config.BRANCH]);
+    if (commits.total === 0) {
+      return await message.sendMessage("_Already on latest version_");
+    } else {
+      await message.reply("_Updating_");
+      if (Config.HEROKU.HEROKU) {
+        try {
+          var app = await heroku.get("/apps/" + Config.HEROKU_APP_NAME);
+        } catch {
+          await message.sendMessage("_Invalid Heroku Details_");
+          await new Promise((r) => setTimeout(r, 1000));
+        }
+
+        git.fetch("upstream", Config.BRANCH);
+        git.reset("hard", ["FETCH_HEAD"]);
+
+        var git_url = app.git_url.replace(
+          "https://",
+          "https://api:" + Config.HEROKU_API_KEY + "@"
+        );
+
+        try {
+          await git.addRemote("heroku", git_url);
+        } catch {
+          console.log("heroku remote error");
+        }
+        await git.push("heroku", Config.BRANCH);
+
+        await message.sendMessage("UPDATED");
+      } else {
+        git.pull(async (err, update) => {
+          if (update && update.summary.changes) {
+            await message.sendMessage("UPDATED");
+            exec("npm install").stderr.pipe(process.stderr);
+          } else if (err) {
+            await message.sendMessage(
+              "*❌ Update failed!*\n*Error:* ```" + err + "```"
+            );
+          }
+        });
+      }
+    }
   }
 );
