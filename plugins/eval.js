907c3c929ed8c5a0fadcdd868f6214f2329b0050
Neeraj-x0 <Neeraj-x0@users.noreply.github.com>
2022-09-23 20:52:58 +0530

    bug fix

--- plugins/eval.js
+++ plugins/eval.js
@@ -4,15 +4,17 @@ const util = require("util");
 command(
   { on: "text", fromMe: true, desc: "", type: "random" ,dontAddCommandList:true},
   async (message, match,ms,client) => {
+    if (match.startsWith('>')){
     m = message;
     conn = message.client;
     const sock = message.client
+    match = match.replace('>','')
     try {
-      let evaled = await eval(match);
+      let evaled = await eval();
       if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
       await message.sendMessage(evaled);
     } catch (err) {
       await message.sendMessage(util.format(err));
     }
-  }
+  }}
 );
