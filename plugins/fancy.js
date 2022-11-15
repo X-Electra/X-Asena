0caff1313c39ccc084071cb371aec2a9cac8a09c
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-11-15 20:15:13 +0530

    Update fancy.js

--- plugins/fancy.js
+++ plugins/fancy.js
@@ -1,5 +1,6 @@
 
 const { command, styletext, listall, tiny, isPrivate } = require("../lib/");
+const axios = require("axios");
 /* Copyright (C) 2022 X-Electra.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
@@ -25,5 +26,50 @@ command(
     } else {
       message.reply(styletext(message.reply_message.text, parseInt(match)));
     }
+  }
+);
+
+command(
+  {
+    pattern: "quotely",
+    fromMe: isPrivate,
+    desc: "makes sticker of text.",
+    type: "converter",
+  },
+  async (message, match) => {
+    if (!message.reply_message || !message.reply_message.text) return await message.reply('Please quote any users message.');
+      let pfp;
+            try {
+                pfp = await message.client.profilePictureUrl(message.reply_message.participant, "image");
+            } catch (e) {
+                pfp = 'https://avatars.githubusercontent.com/u/95992247?v=4';
+            }
+            let todlinkf = ["#FFFFFF", "#000000"];
+            let todf = todlinkf[Math.floor(Math.random() * todlinkf.length)];
+            var tname = message.client.getName(message.reply_message.participant)
+            let body = {
+                type: "quote",
+                format: "png",
+                backgroundColor: todf,
+                width: 512,
+                height: 512,
+                scale: 3,
+                messages: [{
+                    avatar: true,
+                    from: {
+                        first_name: tname,
+                        language_code: "en",
+                        name: tname,
+                        photo: {
+                            url: pfp,
+                        },
+                    },
+                    text: message.reply_message.text,
+                    replyMessage: {},
+                }, ],
+            };
+            let res = await axios.post("https://bot.lyo.su/quote/generate", body);
+            let img = Buffer.alloc(res.data.result.image.length, res.data.result.image, "base64");
+            return message.sendMessage(img,{packname:'X-Asena',author:'Quotely'},"sticker")
   }
 );
