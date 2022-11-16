66565083602f7f15543267fd16e148f5866d749e
Sam Pandey <104565822+SamPandey001@users.noreply.github.com>
2022-11-16 09:49:17 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -84,6 +84,8 @@ async function Xasena() {
 
   conn.ev.on("connection.update", async (s) => {
     const { connection, lastDisconnect } = s;
+    console.log(s)
+    console.log(state)
     if (connection === "connecting") {
       console.log("X-Asena");
       console.log("ℹ️ Connecting to WhatsApp... Please Wait.");
