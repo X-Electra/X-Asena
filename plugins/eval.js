539a5c336a29c6a94ace1a1344e4ab85dffcbf18
Neeraj-x0 <krishnaneeraj774@gmail.com>
2023-06-15 21:01:58 +0530

    Optifine Beta

--- plugins/eval.js
+++ plugins/eval.js
@@ -10,7 +10,6 @@ const config = require("../config");
 
 command({pattern:'eval', on: "text", fromMe: true,desc :'Runs a server code'}, async (message, match, m, client) => {
   if (match.startsWith(">")) {
-    //const m = message;
     try {
       let evaled = await eval(`${match.replace(">", "")}`);
       if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
