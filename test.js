5f2e0c856ccad34cc70bedc7d2983c782622267f
Neeraj-x0 <krishnaneeraj774@gmail.com>
2023-08-03 23:11:02 +0530

    stream Error Fixed

--- test.js
+++
@@ -1,8 +0,0 @@
-const { Configuration, OpenAIApi } = require("openai");
-const configuration = new Configuration({
-  apiKey: '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',
-});
-const openai = new OpenAIApi(configuration);
-openai.listModels().then((a) => {
-  console.log(JSON.stringify(a.data,null,2));
-});
