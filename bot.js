20179b826571bb08b210b82061c3ddc432bb42d5
Neeraj <contact@neerajx0.xyz>
2024-05-05 18:30:37 +0530

    chore: Update index.js to use @neeraj-x0/nodetoolkit for starting the bot

---
+++ bot.js
@@ -0,0 +1,45 @@
+const fs = require("fs").promises;
+const path = require("path");
+const config = require("./config");
+const connect = require("./lib/connection");
+const { getandRequirePlugins } = require("./assets/database/plugins");
+const { getJson } = require("./lib");
+
+global.__basedir = __dirname;
+
+const readAndRequireFiles = async (directory) => {
+  try {
+    const files = await fs.readdir(directory);
+    return Promise.all(
+      files
+        .filter((file) => path.extname(file).toLowerCase() === ".js")
+        .map((file) => require(path.join(directory, file)))
+    );
+  } catch (error) {
+    console.error("Error reading and requiring files:", error);
+    throw error;
+  }
+};
+
+async function initialize() {
+ 
+  console.log("X-Asena");
+  try {
+    await readAndRequireFiles(path.join(__dirname, "/assets/database/"));
+    console.log("Syncing Database");
+
+    await config.DATABASE.sync();
+
+    console.log("⬇  Installing Plugins...");
+    await readAndRequireFiles(path.join(__dirname, "/assets/plugins/"));
+    await getandRequirePlugins();
+    console.log("✅ Plugins Installed!");
+
+    await connect();
+  } catch (error) {
+    console.error("Initialization error:", error);
+    process.exit(1); // Exit with error status
+  }
+}
+
+initialize();
