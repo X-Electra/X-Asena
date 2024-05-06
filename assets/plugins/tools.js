747988d3c5535f1a8e441a8a6b8e496cec915b60
TOXIC ALIEN <64305844+Alien-Alfa@users.noreply.github.com>
2024-05-06 00:38:03 -0700

    Status Saver Added!
    Status saver tool for user. Supports Image, Video, Audio and text

--- assets/plugins/tools.js
+++ assets/plugins/tools.js
@@ -23,6 +23,29 @@ command(
   }
 );
 
+// STATUS SAVER ( MAKE fromMe: false TO USE AS PUBLIC )
+command(
+  { 
+    on: "text",
+    fromMe: true,
+    desc: "Save or Give Status Updates",
+    dontAddCommandList: true,
+    type: "Tool"
+  },
+  async (message, match, m) => {
+    try{
+      if (message.isGroup) return;
+      const triggerKeywords = ["save", "send", "sent", "snt", "give", "snd"];
+      const cmdz = match.toLowerCase().split(' ')[0];
+      if (triggerKeywords.some(tr => cmdz.includes(tr))) {
+        const relayOptions = { messageId: m.quoted.key.id };
+        return await message.client.relayMessage(message.jid, m.quoted.message, relayOptions);
+      }
+    } catch (error) {
+      console.error("[Error]:", error);
+    }
+  });
+
 command(
   {
     pattern: "qr",
