f644796b8b5878c7ec7dc691a96d8e431ce783be
Neeraj <contact@neerajx0.xyz>
2024-05-06 16:08:28 +0530

    feat: Add support for downloading audio and video from YouTube

--- assets/plugins/ytdl.js
+++ assets/plugins/ytdl.js
@@ -6,7 +6,7 @@ const {
   getBuffer,
   toAudio,
 } = require("../../lib");
-const { yta, ytv } = require("../../lib/ytdl");
+const { yta, ytv, ytsdl } = require("../../lib/ytdl");
 
 command(
   {
@@ -44,7 +44,7 @@ command(
     match = match || message.reply_message.text;
     if (!match) return await message.reply("Give me a youtube link");
     if (!isUrl(match)) return await message.reply("Give me a youtube link");
-    let { dlink, title } = await ytv(match,'360p');
+    let { dlink, title } = await ytv(match, "360p");
     await message.reply(`_Downloading ${title}_`);
     return await message.sendMessage(
       message.jid,
@@ -55,5 +55,36 @@ command(
       },
       "video"
     );
+  }
+);
+
+command(
+  {
+    pattern: "song",
+    fromMe: isPrivate,
+    desc: "Download audio from youtube",
+  },
+  async (message, match) => {
+    match = match || message.reply_message.text;
+    if (!match) return await message.reply("Give me a query");
+    let { dlink, title } = await ytsdl(match);
+    await message.reply(`_Downloading ${title}_`);
+    let buff = await getBuffer(dlink);
+    return await message.sendMessage(message.jid, buff, "audio");
+  }
+);
+
+command(
+  {
+    pattern: "video",
+    fromMe: isPrivate,
+    desc: "Download video from youtube",
+  },
+  async (message, match) => {
+    match = match || message.reply_message.text;
+    if (!match) return await message.reply("Give me a query");
+    let { dlink, title } = await ytsdl(match, "video");
+    await message.reply(`_Downloading ${title}_`);
+    return await message.sendMessage(message.jid, dlink, "video");
   }
 );
