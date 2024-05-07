0039638e2b9e5ffbc5abbc19c938e86d352ac5a0
Neeraj <contact@neerajx0.xyz>
2024-05-07 11:12:17 +0530

    Refactor lib/index.js to add readQr and getLyrics functions

--- assets/plugins/tools.js
+++ assets/plugins/tools.js
@@ -1,15 +1,13 @@
 const {
   command,
-  isPrivate,
   qrcode,
   Bitly,
   isUrl,
-  readQR,
-  AItts,
-  getJson,
+  readQr,
 } = require("../../lib/");
 
 const { downloadMediaMessage } = require("@whiskeysockets/baileys");
+const { getLyrics } = require("../../lib/functions");
 command(
   {
     pattern: "vv",
@@ -51,7 +49,7 @@ command(
         reuploadRequest: message.client.updateMediaMessage,
       }
     );
-    readQR(buffer)
+    readQr(buffer)
       .then(async (data) => {
         return await message.sendMessage(message.jid, data);
       })
@@ -64,7 +62,7 @@ command(
 
 command(
   {
-    pattern: "bitly ?(.*)",
+    pattern: "bitly",
     fromMe: true,
     desc: "Converts Url to bitly",
     type: "tool",
@@ -80,29 +78,6 @@ command(
 
 command(
   {
-    pattern: "tts",
-    fromMe: true,
-    desc: "Convert text to speech",
-  },
-  async (message, match) => {
-    try {
-      const text = match || message.reply_message.text;
-      if (!text) {
-        await message.reply("Please provide the text for TTS.");
-        return;
-      }
-      await AItts(message, text);
-    } catch (error) {
-      console.error("Error processing TTS command:", error);
-    }
-  }
-);
-
-const API_KEY = "e6d0cd0023b7ee562a97be33d3c5f524";
-const BASE_URL = "https://api.musixmatch.com/ws/1.1/";
-
-command(
-  {
     pattern: "lyric",
     fromMe: true,
     desc: "Searches for lyrics based on the format: song;artist",
@@ -114,60 +89,19 @@ command(
       await message.reply("Search with this format: \n\t_lyric song;artist_");
     } else {
       try {
-        let trackId = null;
-
-        const searchUrl = `${BASE_URL}track.search?q_track=${encodeURIComponent(
-          song
-        )}&q_artist=${encodeURIComponent(
-          artist
-        )}&f_has_lyrics=1&apikey=${API_KEY}`;
-        console.log(searchUrl);
-        const searchData = await getJson(searchUrl);
-
-        const trackList = searchData.message.body.track_list;
-
-        if (trackList.length > 0) {
-          trackId = trackList[0].track.track_id;
-          
-        } else {
-          const allTracksUrl = `${BASE_URL}track.search?q_artist=${encodeURIComponent(
-            artist
-          )}&apikey=${API_KEY}`;
-          console.log(allTracksUrl);
-          const allTracksData = await getJson(allTracksUrl);
-
-          const allTracks = allTracksData.message.body.track_list;
-
-          if (allTracks.length > 0) {
-            trackId = allTracks[0].track.track_id;
-          }
-        }
-
-        if (trackId) {
-          const lyricsUrl = `${BASE_URL}track.lyrics.get?track_id=${trackId}&apikey=${API_KEY}`;
-          console.log(lyricsUrl);
-          const lyricsData = await getJson(lyricsUrl);
-
-          let lyrics = lyricsData.message.body.lyrics.lyrics_body;
-          const disclaimer =
-            "******* This Lyrics is NOT for Commercial use *******";
-          lyrics = lyrics.replace(disclaimer, "");
-
-          const data = {
-            artist_name: artist,
-            song: song,
-            lyrics: lyrics.replace(/\(\d+\)$/, ""),
-          };
-
-          return await message.reply(`*Artist:* ${data.artist_name}\n*Song:* ${data.song}\n*Lyrics:*\n${data.lyrics.trim()}
-          `);
+        const data = await getLyrics(song, artist);
+        if (data) {
+          return await message.reply(
+            `*Artist:* ${data.artist_name}\n*Song:* ${
+              data.song
+            }\n*Lyrics:*\n${data.lyrics.trim()}`
+          );
         } else {
           return await message.reply(
             "No lyrics found for this song by this artist."
           );
         }
       } catch (error) {
-        console.error("Error:", error);
         return await message.reply("An error occurred while fetching lyrics.");
       }
     }
