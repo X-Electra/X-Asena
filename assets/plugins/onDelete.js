795db6bc9e2b9ccf2eea3d4c08c7e86025c5cac0
Neeraj <contact@neerajx0.xyz>
2024-05-13 22:33:56 +0530

    refactor: Fix typo in onDelete.js message template

--- assets/plugins/onDelete.js
+++ assets/plugins/onDelete.js
@@ -33,7 +33,7 @@ command(
     }
     return await message.sendMessage(
       DELETED_LOG_CHAT,
-      `_Message Deleted_\n_From : ${msg.from}_\n${name}+\n_SenderJid : ${msg.sender}_`,
+      `_Message Deleted_\n_From : ${msg.from}_\n${name}\n_SenderJid : ${msg.sender}_`,
       { quoted: deleted }
     );
   }
