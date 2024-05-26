b2581ac2f5858b273882ca11c38cbd84ec7569b7
Neeraj <contact@neerajx0.xyz>
2024-05-26 21:29:45 +0530

    refactor: Update phone number parsing in pcode.js

--- pcode.js
+++ pcode.js
@@ -61,7 +61,7 @@ const startSock = async () => {
     if (!sock.authState.creds.me?.id && !restarted) {
       let phoneNumber = await question("Enter phone number: ");
       try {
-        phoneNumber = phonenumber.parsePhoneNumber(phoneNumber);
+        phoneNumber = phonenumber.parsePhoneNumber(phoneNumber).replace("+", "");
       } catch (error) {
         console.log("Invalid phone number");
         process.exit(1);
