b8a2dcfeea2c2aa66e13a2f24c74184c8021e4ca
Neeraj <contact@neerajx0.xyz>
2024-06-12 13:42:11 +0530

    refactor: Update eslint.config.mjs to include "no-unused-vars" rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import { rules } from "@eslint/js/src/configs/eslint-all";
 
 
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
+  rules["no-unused-vars"],
 ];
