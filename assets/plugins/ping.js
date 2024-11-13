2aad022db35ea65057e8d29078fdcb18011064d0
Neeraj-x0 <krishnaneeraj773@gmail.com>
2024-11-13 23:24:41 +0530

    feat: update ping command responses for clarity and engagement

--- assets/plugins/ping.js
+++ assets/plugins/ping.js
@@ -4,17 +4,16 @@ command(
   {
     pattern: "ping",
     fromMe: isPrivate,
-    desc: "To check ping",
+    desc: "To check if the bot is awake",
     type: "user",
   },
   async (message, match) => {
     const start = new Date().getTime();
-    await message.sendMessage(message.jid, "```Ping!```");
+    await message.sendMessage(message.jid, "```Is this thing on?```");
     const end = new Date().getTime();
     return await message.sendMessage(
       message.jid,
-      "*Pong!*\n ```" + (end - start) + "``` *ms*"
+      "*Boing!*\n ```" + (end - start) + "``` *milliseconds of my life wasted*"
     );
   }
 );
-
